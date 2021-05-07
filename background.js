9a44b969caedce8b8f3f02b391343d599850ba24
Roman Furman <roman.furman@uptech.team>
2021-05-07 17:02:24 +0300

    popup & manifest 2 (for DOM parsing)

--- background.js
+++ background.js
@@ -1,7 +1,29 @@
-let defaultAccount = "1";
+let defaultAccount = 0;
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ defaultAccount });
   console.log(`Default Account color set to ${defaultAccount}`);
 });
 
+chrome.runtime.onMessage.addListener((data, sender, sendResponse) => {
+  const url = 'https://accounts.google.com/ListAccounts?gpsia=1&source=ogb&mo=1&origin=https://accounts.google.com'
+  fetch(url)
+      .then(response => (
+          response.text()
+      ))
+      .then(function(rawText) {
+        const parser = new DOMParser();
+        const html = parser.parseFromString(rawText, 'application/xml').querySelector('script').innerHTML
+        return html.split('\'')[1]
+          .replace(/\\x([0-9a-fA-F]{2})/g, (match, paren) => (
+              String.fromCharCode(parseInt(paren, 16))
+          ))
+          .replace(/\\\//g, '\/')
+          .replace(/\\n/g, '')
+      })
+      .then(text => (
+          JSON.parse(text)
+      ))
+      .then(sendResponse);
+  return true
+});
--- popup.js
+++ popup.js
@@ -1,46 +1,82 @@
-// Initialize butotn with users's prefered color
-const changeColor = document.getElementById("changeColor");
+function signIn(email) { // A builder that returns a function to set the url to allow the user to log in to a signed out account
+  return (responses) => {
+      let response = responses[0]
+        , url;
+      if (response.url.includes('google')) { // Weak test for if Google will redirect to that url
+          url = new URL(response.url);
+      } else {
+          url = new URL('https://www.google.com/webhp');
+      }
+      let params = new URLSearchParams(url.search);
 
-chrome.storage.sync.get("defaultAccount", ({ defaultAccount }) => {
-  changeColor.textContent = defaultAccount;
-  console.log("defaultAccount set : ", defaultAccount);
-});
+      // This empties the authuser and moves it to the end to model the format that Google uses for this
+      params.delete('authuser'); 
+      params.set('authuser', '');
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  console.log("changeColor button clicked, target: ");
+      url.search = params.toString();
+      let newURL = 'https://accounts.google.com/AccountChooser?source=ogb&continue='+encodeURIComponent(url.toString())+'&Email='+email;
+      
+      window.open(newURL);
+      window.close(); // Just in case
+      
+  }
+}
 
-  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  var tabUrl = tab.url; //encodeURIComponent(tab.url);
-  var tabTitle = tab.title; //encodeURIComponent(tab.title);
-
-  console.log("tabUrl: ", tabUrl);
-  console.log("tabTitle: ", tabTitle);
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
-});
-
-// The body of this function will be execuetd as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  // const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  // var tabUrl = encodeURIComponent(tab.url);
-  // var tabTitle = encodeURIComponent(tab.title);
-  // console.log("tabUrl: ", tabUrl);
-  // console.log("tabTitle: ", tabTitle);
-  // var myNewUrl =
-  //   "https://www.mipanga.com/Content/Submit?url=" +
-  //   tabUrl +
-  //   "&title=" +
-  //   tabTitle;
-  // //Update the url here.
-  // chrome.tabs.update(tab.id, { url: myNewUrl });
-
-  chrome.storage.sync.get("defaultAccount", ({ defaultAccount }) => {
-    document.body.style.backgroundColor = "red";
+function populate(response) { // Create the GUI from the strange nested Array structure that Google accounts responds with
+  chrome.storage.sync.get("defaultAccount", (data) => {
+    const defaultAccount = data.defaultAccount
+    response[1].forEach(info => { // There is useless info in response[0]; response[1] has the accounts
+      let a = document.createElement('a');
+      a.classList.add('topA')
+      
+      let img = document.createElement('img');
+      img.classList.add('img')
+      img.src = info[4]; // Profile image URL
+      a.appendChild(img);
+      
+      let topDiv = document.createElement('div');
+      topDiv.classList.add('top')
+      
+      let nameDiv = document.createElement('div');
+      nameDiv.classList.add('name')
+      nameDiv.appendChild(document.createTextNode(info[2])); // Name
+      topDiv.appendChild(nameDiv);
+      
+      let emailDiv = document.createElement('div');
+      emailDiv.classList.add('email')
+      emailDiv.appendChild(document.createTextNode(info[3])); // Email
+      topDiv.appendChild(emailDiv);
+      
+      a.appendChild(topDiv);
+      
+      if (info.length < 16) { // If the account is signed out (as far as I know)
+          let cornerDiv = document.createElement('div');
+          cornerDiv.classList.add('corner')
+          cornerDiv.appendChild(document.createTextNode('Signed out'));
+          a.appendChild(cornerDiv);
+          a.addEventListener('click', () => {
+            chrome.tabs.query({ // Get current tab
+              active: true,
+              currentWindow: true
+          }, signIn(info[3])); // Navigate to signin
+          });
+      } else {
+          if (info[7] == defaultAccount) { // Account index (pretty sure)
+              let cornerDiv = document.createElement('div');
+              cornerDiv.classList.add('corner')
+              cornerDiv.appendChild(document.createTextNode('Selected'));
+              a.appendChild(cornerDiv);
+          }
+          a.addEventListener("click", async () => {
+            chrome.storage.sync.set({ defaultAccount: info[7] }, function() {
+              window.close();
+            });
+          
+          })
+      }
+      document.body.appendChild(a);
+    });
   });
 }
+
+chrome.runtime.sendMessage(null, populate);
