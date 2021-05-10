adb3f0e5adb40767c90324b41d7f6ef35885e772
Andriy Bas <andriy.bas@uptech.team>
2021-05-10 14:47:06 +0300

    add comment

--- utils.js
+++ utils.js
@@ -1,3 +1,4 @@
+// Full list of Google Services subdomains - https://gist.github.com/abuvanth/b9fcbaf7c77c2954f96c6e556138ffe8
 function isGoogleServiceUrl(url) {
   return /https?:\/\/.*(?:mail|drive|calendar|meet|docs|admin|photos|translate|keep|hangouts|chat|currents|maps|news|ads|ediscovery|jamboard|earth|travel|podcasts|classroom|business|myaccount|adsense|cloud|adwords|analytics)\.google\.co.*/i.test(
     url
