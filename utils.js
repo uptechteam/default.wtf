7f72deaf10a80e99eaa34656522619117000bd24
Andriy Bas <andriy.bas@uptech.team>
2021-05-11 17:56:44 +0300

    remove Travel, change Currents to Workspace

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 // Full list of Google Services subdomains - https://gist.github.com/abuvanth/b9fcbaf7c77c2954f96c6e556138ffe8
 function isGoogleServiceUrl(url) {
-  return /^https?:\/\/[^?&]*(?:mail|drive|calendar|meet|docs|admin|photos|translate|keep|hangouts|chat|currents|maps|news|ads|ediscovery|jamboard|earth|travel|podcasts|classroom|business|myaccount|adsense|cloud|adwords|analytics)\.google\.co.*/i.test(
+  return /^https?:\/\/[^?&]*(?:mail|drive|calendar|meet|docs|admin|photos|translate|keep|hangouts|chat|workspace|maps|news|ads|ediscovery|jamboard|earth|podcasts|classroom|business|myaccount|adsense|cloud|adwords|analytics)\.google\.co.*/i.test(
     url
   );
 }
@@ -112,8 +112,8 @@ function allSupportedGoogleServices() {
       url: "chat.google.com",
     },
     {
-      name: "Currents",
-      url: "currents.google.com",
+      name: "Workspace",
+      url: "workspace.google.com",
     },
     {
       name: "News",
@@ -135,10 +135,6 @@ function allSupportedGoogleServices() {
       name: "Earth",
       url: "earth.google.com",
       img: "./images/logos/earth.png",
-    },
-    {
-      name: "Travel",
-      url: "travel.google.com",
     },
     {
       name: "Podcasts",
